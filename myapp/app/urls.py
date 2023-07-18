d30ddd3d82602756d404224289e06a759b51096d
정승일 <jungsi1217@naver.com>
2023-07-18 16:31:45 +0900

    edit,delete

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import index, bloglist, blogdetails, blogwrite, create
+from blog.views import index, bloglist, blogdetails, blogwrite, create, edit, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
+    path('',Index),
     path('bloglist/',bloglist),
     path('bloglist/<int:pk>',blogdetails, name='blogdetails'),
     path('blogwrite/',blogwrite),
-    path('blog/create/',create, name="create")
+    path('blog/create/',create, name="create"),
+    path('bloglist/<int:pk>/edit/',edit, name="edit"),
+    path('bloglist/<int:pk>/delete/',delete,name="delete")
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Content
+
 def index(request):
     return render(request, 'blog/index.html')
 
@@ -11,6 +12,8 @@ def bloglist(request):
     }
     return render(request, 'blog/bloglist.html', context)
 
+
+
 def blogdetails(request, pk):
     content = Content.objects.get(pk=pk)
 
@@ -19,8 +22,12 @@ def blogdetails(request, pk):
     }
     return render(request, 'blog/blogdetail.html', context)
 
+
+
 def blogwrite(request):
     return render(request, 'blog/write.html')
+
+
 
 def create(request):
     content = Content()
@@ -28,3 +35,23 @@ def create(request):
     content.content = request.GET['content']
     content.save()
     return redirect('/bloglist/' + str(content.pk))
+
+
+def edit(request,pk):
+    content = Content.objects.get(pk=pk)
+    if request.method == 'POST':
+        content.title = request.POST['title']
+        content.content = request.POST['content']
+
+        content.save()
+        return redirect('/bloglist/' + str(content.pk))
+    else:
+        content = Content.objects.all()
+        return render(request, 'blog/edit.html')
+    
+
+def delete(request,pk):
+    content = Content.objects.get(pk=pk)
+    content.delete()
+    return redirect('/bloglist/')
+    
